087e412c10fe164f1108814e70d3ad29a3d45405
EduardoZortea18 <eduardozortea9@gmail.com>
2024-06-02 17:22:48 -0300

    feat: adding handler package

--- router/routes.go
+++ router/routes.go
@@ -1,43 +1,17 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/EduardoZortea18/opsApi/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/openings/:id", func(ctx *gin.Context) {
-			id := ctx.Param("id")
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": id,
-			})
-		})
-
-		v1.POST("/openings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "second route",
-			})
-		})
-
-		v1.DELETE("/openings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "third route",
-			})
-		})
-
-		v1.PUT("/openings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "fourth route",
-			})
-		})
-
-		v1.GET("/openings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "fifth route",
-			})
-		})
+		v1.GET("/openings/:id", handler.GetByIdOpeningHandler)
+		v1.POST("/openings", handler.CreateOpeningHandler)
+		v1.DELETE("/openings", handler.DelteOpeningHandler)
+		v1.PUT("/openings", handler.EditOpeningHandler)
+		v1.GET("/openings", handler.GetOpeningHandler)
 	}
 }
